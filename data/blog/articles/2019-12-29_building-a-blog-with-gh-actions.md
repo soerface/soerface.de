f66258d3664b6172607a40c074475d2ab0f8290c
Soeren Wegener <wegener92@gmail.com>
2019-12-26 02:33:19 +0100

    Add frontmatter to articles

---
+++ data/blog/articles/2019-12-29_building-a-blog-with-gh-actions.md
@@ -0,0 +1,10 @@
+---
+devto_post_id: 12345
+title: Hello world
+---
+
+asdf gg
+
+## And what now?
+
+**YEAH** well formatting is working at least
--- src/sections/blog.py
+++ src/sections/blog.py
@@ -1,8 +1,27 @@
-from flask import Blueprint, render_template
+from pathlib import Path
+
+from flask import Blueprint, render_template, Markup
+from markdown import markdown
+import frontmatter
+from datetime import datetime
 
 bp = Blueprint('blog', __name__, url_prefix='/blog', template_folder='templates')
 
 
+def load_article(path: Path):
+    article = frontmatter.load(path)
+    date_string = path.name.partition('_')[0]
+    article.metadata['date'] = datetime.strptime(date_string, '%Y-%m-%d')
+    # frontmatter.dump(article, path)
+    return {
+        'metadata': article.metadata,
+        'content': Markup(markdown(article.content))
+    }
+
+
 @bp.route('/')
 def index():
-    return render_template('blog/index.html')
+    paths = Path('data/blog/articles/').glob('**/*.md')
+    # event_list = [re.search(r'(\d+)/(.+).yml', str(p)).groups() for p in paths]
+    articles = [load_article(x) for x in paths]
+    return render_template('blog/index.html', article_list=articles)
