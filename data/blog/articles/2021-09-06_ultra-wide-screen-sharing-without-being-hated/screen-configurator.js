010ddf2af9027bf2f76e497c506b97b5329c1ff9
Soeren Wegener <wegener92@gmail.com>
2021-10-03 15:21:09 +0200

    Screen configurator: Revert accidental replacement of parseInt()

--- data/blog/articles/2021-09-06_ultra-wide-screen-sharing-without-being-hated/screen-configurator.js
+++ data/blog/articles/2021-09-06_ultra-wide-screen-sharing-without-being-hated/screen-configurator.js
@@ -263,9 +263,9 @@ function hexToRgb(hex) {
     // https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
     const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result ? {
-        r: Math.floor(result[1], 16) / 255,
-        g: Math.floor(result[2], 16) / 255,
-        b: Math.floor(result[3], 16) / 255,
+        r: parseInt(result[1], 16) / 255,
+        g: parseInt(result[2], 16) / 255,
+        b: parseInt(result[3], 16) / 255,
     } : null;
 }
 
